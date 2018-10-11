a46f4cce04802622929f242a13ffc62ed8d63208
John Hammerlund <johnhammerlund@gmail.com>
2018-10-11 12:56:35 -0700

    Boilerplate project setup

---
+++ Sources/Relay/Relay.h
@@ -0,0 +1,18 @@
+//
+//  Relay.h
+//  Relay
+//
+//  Created by John Hammerlund on 10/11/18.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Relay.
+FOUNDATION_EXPORT double RelayVersionNumber;
+
+//! Project version string for Relay.
+FOUNDATION_EXPORT const unsigned char RelayVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Relay/PublicHeader.h>
+
+
