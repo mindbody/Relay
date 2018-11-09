e42cd054542ee9169519634cb585edf5f1acbdf9
John Hammerlund <johnhammerlund@gmail.com>
2018-11-09 15:36:01 -0800

    More documentation

--- RelayExampleIOS/RelayExampleIOSUITests/RelayExampleIOSUITests.swift
+++ RelayExampleIOS/RelayExampleIOSUITests/RelayExampleIOSUITests.swift
@@ -11,14 +11,25 @@ import Relay
 
 final class RelayExampleIOSUITests: XCTestCase {
 
+    /// Sample Test Case
+    ///
     func testDisplaysRecipe() {
         let app = XCUIApplication()
+
+        /// A DependencyInstructionLaunchArgument pre-formats dynamic injection instructions.
+        /// In this example, we're sending a command line argument to inject a specific `RecipeServiceStub`
+        /// for the global `RecipeServiceType`. This allows us to make focused, consistent assertions on the UI.
 
         let injectionLaunchArgument = DependencyInstructionLaunchArgument(type: .recipeService, factory: .testRecipeService)
         let builder = LaunchArgumentBuilder(arguments: [injectionLaunchArgument])
         app.launchArguments = builder.build()
 
+        /// GIVEN I launch the app
+        
         app.launch()
+
+        /// WHEN the recipe screen displays
+        /// THEN I should see a list of itemized ingredients and instructions
 
         let nameLabel = app.staticTexts["recipe-details.label.name"]
         let ingredientsLabel = app.staticTexts["recipe-details.label.ingredients-list"]
