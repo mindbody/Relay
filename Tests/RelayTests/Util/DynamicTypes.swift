69af3b7bf248f2f37c4c4efbdc4444d1d9967df9
John Hammerlund <johnhammerlund@gmail.com>
2019-06-18 14:07:50 -0700

    Remove semicolons

--- Tests/RelayTests/Util/DynamicTypes.swift
+++ Tests/RelayTests/Util/DynamicTypes.swift
@@ -12,24 +12,24 @@ protocol ConstructableDynamicType: class {
 }
 
 final class DynamicTypes {
-    final class TypeA: ConstructableDynamicType {}; final class TypeB: ConstructableDynamicType {};
-    final class TypeC: ConstructableDynamicType {}; final class TypeD: ConstructableDynamicType {};
-    final class TypeE: ConstructableDynamicType {}; final class TypeF: ConstructableDynamicType {};
-    final class TypeG: ConstructableDynamicType {}; final class TypeH: ConstructableDynamicType {};
-    final class TypeI: ConstructableDynamicType {}; final class TypeJ: ConstructableDynamicType {};
-    final class TypeK: ConstructableDynamicType {}; final class TypeL: ConstructableDynamicType {};
-    final class TypeM: ConstructableDynamicType {}; final class TypeN: ConstructableDynamicType {};
-    final class TypeO: ConstructableDynamicType {}; final class TypeP: ConstructableDynamicType {};
-    final class TypeQ: ConstructableDynamicType {}; final class TypeR: ConstructableDynamicType {};
-    final class TypeS: ConstructableDynamicType {}; final class TypeT: ConstructableDynamicType {};
-    final class TypeU: ConstructableDynamicType {}; final class TypeV: ConstructableDynamicType {};
-    final class TypeW: ConstructableDynamicType {}; final class TypeX: ConstructableDynamicType {};
-    final class TypeY: ConstructableDynamicType {}; final class TypeZ: ConstructableDynamicType {};
-    final class Type1: ConstructableDynamicType {}; final class Type2: ConstructableDynamicType {};
-    final class Type3: ConstructableDynamicType {}; final class Type4: ConstructableDynamicType {};
-    final class Type5: ConstructableDynamicType {}; final class Type6: ConstructableDynamicType {};
-    final class Type7: ConstructableDynamicType {}; final class Type8: ConstructableDynamicType {};
-    final class Type9: ConstructableDynamicType {}; final class Type0: ConstructableDynamicType {};
+    final class TypeA: ConstructableDynamicType {}; final class TypeB: ConstructableDynamicType {}
+    final class TypeC: ConstructableDynamicType {}; final class TypeD: ConstructableDynamicType {}
+    final class TypeE: ConstructableDynamicType {}; final class TypeF: ConstructableDynamicType {}
+    final class TypeG: ConstructableDynamicType {}; final class TypeH: ConstructableDynamicType {}
+    final class TypeI: ConstructableDynamicType {}; final class TypeJ: ConstructableDynamicType {}
+    final class TypeK: ConstructableDynamicType {}; final class TypeL: ConstructableDynamicType {}
+    final class TypeM: ConstructableDynamicType {}; final class TypeN: ConstructableDynamicType {}
+    final class TypeO: ConstructableDynamicType {}; final class TypeP: ConstructableDynamicType {}
+    final class TypeQ: ConstructableDynamicType {}; final class TypeR: ConstructableDynamicType {}
+    final class TypeS: ConstructableDynamicType {}; final class TypeT: ConstructableDynamicType {}
+    final class TypeU: ConstructableDynamicType {}; final class TypeV: ConstructableDynamicType {}
+    final class TypeW: ConstructableDynamicType {}; final class TypeX: ConstructableDynamicType {}
+    final class TypeY: ConstructableDynamicType {}; final class TypeZ: ConstructableDynamicType {}
+    final class Type1: ConstructableDynamicType {}; final class Type2: ConstructableDynamicType {}
+    final class Type3: ConstructableDynamicType {}; final class Type4: ConstructableDynamicType {}
+    final class Type5: ConstructableDynamicType {}; final class Type6: ConstructableDynamicType {}
+    final class Type7: ConstructableDynamicType {}; final class Type8: ConstructableDynamicType {}
+    final class Type9: ConstructableDynamicType {}; final class Type0: ConstructableDynamicType {}
 
     static var allTypes: [ConstructableDynamicType.Type] = [
         TypeA.self, TypeB.self, TypeC.self, TypeD.self,
